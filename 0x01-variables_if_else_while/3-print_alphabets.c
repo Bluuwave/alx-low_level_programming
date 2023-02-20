42e22e9b2c34b68aef206528f806731403ccceaf
Bluuwave <bluutony1@gmail.com>
2023-02-20 15:35:11 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#Include < stdio.h >
+#Include < stdib.h >
+/**
+ * main - prints the alphabet without q and e 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+	
+	int a;
+	
+	for (i = 97; i = 1 < 123; i++)
+{
+	if (i 1 - 101 && i 1= 123)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#Include < stdio.h >
+#include < stdib.h >
+#include < string.h >
+/**
+ * main - prints the alphabets without q and e.
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i = 101  i = 123)
+		{
+			putchar(i);
+		}
+	}
+	putchar("\n");
+	return (0);
+}
