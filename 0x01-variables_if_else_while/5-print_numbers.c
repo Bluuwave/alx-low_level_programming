8df58aaa44301767224f146e141616d7fd9dd269
Bluuwave <bluutony1@gmail.com>
2023-02-20 16:05:57 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#Include < stdio.h >
+#Include < stdib.h >
+/**
+ * main - prints numberas from 0 to 9.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar("\n");
+	return (0);
+}
