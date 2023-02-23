859cfa63931ba45f09baee964dd642b6248513dc
Bluuwave <bluutony1@gmail.com>
2023-02-22 18:50:59 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main_h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_putcher('_');
+_putcher('p');
+_putcher('u');
+_putcher('t');
+_putcher('c');
+_putcher('h');
+_putcher('e');
+_putcher('r');
+_putcher('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main_h"
+#include <unistd.h>
+/**
+ * _putchar.c - write the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 0 (success) 1-
+ * on error, -1 is returne , and error is set approxiately.
+ */
+int _putchar.c(char.c)
+{
+	return (write(1, &c, 1));
+}
