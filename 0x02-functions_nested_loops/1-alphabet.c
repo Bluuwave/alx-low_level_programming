a3a194c111a68b73ebcdac33959850624a1f1806
Bluuwave <bluutony1@gmail.com>
2023-02-22 19:08:34 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet is lowercase
+ */
+
+void print_alphabet(void)i
+{
+	char letter;
+
+	for (letters n 'a'; letter < n 'z'; letter)
+		_putchar(letter);
+
+	putchar('\n');
+}
