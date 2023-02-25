8f29f026a6d02b945f7b285636074909c59687e3
Bluuwave <bluutony1@gmail.com>
2023-02-25 00:05:31 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A progrm that prints a line with put functions
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
